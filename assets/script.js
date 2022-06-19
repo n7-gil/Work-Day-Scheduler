de674e2373442762c2dd20d53e0cfe6e165de879
N7 GIL <gilbertoaescobedo@gmail.com>
2022-06-18 21:34:57 -0700

    switched var hours from an array into an let object

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,34 @@
-var todayEl = $("#currentDay");
-var leadEl = $("#lead");
-var mainEl = $(".container");
+let todayEl = $("#currentDay");
+let leadEl = $("#lead");
+let mainEl = $(".container");
 
-const hours = ["09:00am","10:00am","11:00am","12:00pm","01:00pm","02:00pm","03:00pm","04:00pm","05:00pm","06:00pm","07:00pm","08:00pm"]
+let timeHour = {
+"09:00am": "",
+"10:00am": "",
+"11:00am": "",
+"12:00pm": "",
+"01:00pm": "",
+"02:00pm": "",
+"03:00pm": "",
+"04:00pm": "",
+"05:00pm": "",
+"06:00pm": "",
+"07:00pm": "",
+"08:00pm": "",
+}
 
 function displayHour() {
-    for (i = 0; i < hours.length; i++) {
+    for (let hour in timeHour) {
         // DOM element 
         let li = $('<li></li>');
         li.addClass('block');
         let span = $('<span></span>');
         span.attr('id', 'blockTime');
-        span.text(hours[i]);
+        span.text(hour);
 
         let textArea= $('<textarea></textarea>');
         textArea.attr('id',"blockText");
+        textArea.text = timeHour[hour]
 
         let button = $('<button></button>');
         button.attr('id', 'blockBtn');
@@ -26,14 +40,43 @@ function displayHour() {
         li.append(span)
         li.append(textArea)
         li.append(button)
-        mainEl.append(li)
-
-        button.on('click', saveEvent)
+        mainEl.append(li)    
     }
 }
 
-function saveEvent(e) {
-    
-}
+    // ($('blockBtn'.get().forEach(element => {
+//     //     $(element).on('click'), () => {
+//     //         let parent = $(element).parent().get()
+//     //         let time = $(parent).children('.blockTime').text()
+//     //         let msg = $(parent).children('.blockText').val()
+//         }
+
+//     //     console.log($(time) is $(msg));
+//     // }) 
+//     // ))
+// }
+
+
+ // button.on('click', saveEvent)
+
+// button.addEventListener('click', function(event) {
+// event.preventDefault();
+
+// var scheduleDay = {
+//     span: span.value,
+//     textArea: textArea.value.trim()
+// };
+
+// localStorage.setItem("scheduleDay", JSON.stringify(scheduleDay));
+// saveEvent();
+
+// });
+
+// function saveEvent(e) {
+//     var saveDay = JSON.parse(localStorage.getItem("scheduleDay"));
+//     if (saveDay !== null) {
+//       document.querySelector(".saved").textContent = "Saved to scheduler"  
+//     }    
+// }
 
 displayHour()
